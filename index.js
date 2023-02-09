a082e56f08833997463726b9f5b5d3ac5d271827
Võ Minh Trí <vominhtri28032010@gmail.com>
2023-02-09 10:18:41 +0700

    .

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
-const fs = require("fs");
-if (process.env.PORT) fs.writeFileSync("./application.yml", fs.readFileSync("./application.yml", "utf8").replace("{PORT}", process.env.PORT));
+// const fs = require("fs");
+// if (process.env.PORT) fs.writeFileSync("./application.yml", fs.readFileSync("./application.yml", "utf8").replace("{PORT}", process.env.PORT));
