3168bcb0fc09c3620639aeafbeb8c50ecfb16437
Võ Minh Trí <vominhtri28032010@gmail.com>
2023-02-09 09:44:32 +0700

    .

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ const application = fs.readFileSync("./application.yml", "utf8");
 const { spawn } = require("child_process");
 
 if (process.env.PORT) fs.writeFileSync("./application.yml", application.replace("{PORT}", process.env.PORT));
-spawn("java", ["-jar", "Lavalink.jar"])
+spawn("java", ["-jar", "Lavalink.jar"], { stdio: "inherit" });
