c7828ccde74000b5c48d570f5bb55c0b68b10b7d
Võ Minh Trí <vominhtri28032010@gmail.com>
2023-02-09 10:08:37 +0700

    .

--- Dockerfile
+++ Dockerfile
@@ -4,3 +4,4 @@ WORKDIR /src/
 COPY ./* /src/
 RUN npm install
 CMD ["node", "index.js"]
+CMD ["java", "-jar", "Lavalink.jar"]
--- index.js
+++ index.js
@@ -1,6 +1,2 @@
 const fs = require("fs");
-const application = fs.readFileSync("./application.yml", "utf8");
-const { spawn } = require("child_process");
-
 if (process.env.PORT) fs.writeFileSync("./application.yml", fs.readFileSync("./application.yml", "utf8").replace("{PORT}", process.env.PORT));
-spawn("java", ["-jar", "./src/Lavalink.jar"], { stdio: "inherit" });
